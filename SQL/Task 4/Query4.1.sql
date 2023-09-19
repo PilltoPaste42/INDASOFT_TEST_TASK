69c134ae95f721db923f3867b416cdd53d58e17d
PilltoPaste42 <maxs130872@gmail.com>
2023-09-19 19:22:37 +0700

    Переработка задания 4.1

--- SQL/Task 4/Query4.1.sql
+++ SQL/Task 4/Query4.1.sql
@@ -1,20 +1,53 @@
 USE EVENTFRAME;
 GO
 
+DECLARE @batchTypeId NVARCHAR(MAX)
+SELECT @batchTypeId = T.Id
+FROM EventFrameTypes AS T
+WHERE T.Name = 'Партия'
+
+DECLARE @batchTypeIdStr NVARCHAR(MAX) = QUOTENAME(@batchTypeId,'''');
+
+DECLARE @cols NVARCHAR(MAX)
+	
+SELECT @cols = ISNULL(@cols + ', ', '') + QUOTENAME(TV.Name)
+FROM EventFrameTypeValues AS TV
+WHERE TV.EventFrameTypeId = @batchTypeId
+
+
+DECLARE @subquery NVARCHAR(MAX) =
+'
 SELECT 
-	F.Name AS Event,
-	TV.Name AS ValueName,
-	V.ValueText,
-	V.ValueFloat,
-	V.ValueInt,
-	V.ValueDatetime
+	F.Id AS [Id партии],
+	TV.Name AS [Param],
+	COALESCE
+	(
+		CAST(V.ValueInt AS NVARCHAR(100)),
+		CAST(V.ValueFloat AS NVARCHAR(100)),
+		CAST(V.ValueText AS NVARCHAR(100)),
+		CONVERT(NVARCHAR(100), V.ValueDatetime, 104)
+	) AS [Val]
 FROM EventFrames AS F
-JOIN EventFrameTypes AS T
-	ON T.Id = F.EventFrameTypeId
 JOIN EventFrameTypeValues AS TV
-	ON TV.EventFrameTypeId = T.Id
+	ON TV.EventFrameTypeId = F.EventFrameTypeId
 JOIN EventFrameValues AS V
 	ON V.EventFrameId = F.Id
 	AND V.UserfieldId = TV.Id
+WHERE F.EventFrameTypeId = ' + @batchTypeIdStr + '
+'
 
-ORDER BY Event
+DECLARE @query NVARCHAR(MAX) =
+'
+SELECT * FROM
+(
+	' + @subquery + '
+) AS Result
+PIVOT
+(
+	MAX(Val) FOR Param IN (' + @cols + ')
+) AS pvt1
+ORDER BY [Номер партии]
+
+'
+EXEC (@query)
+--EXEC (@subquery)
