3fd64e948f5e363dbf3a6a5e0803d7ceb625f70a
PilltoPaste42 <maxs130872@gmail.com>
2023-09-29 13:50:30 +0700

    Настрока подключения к API сервера

--- SampleAnalysis/src/SampleAnalysis.WebUI/vite.config.ts
+++ SampleAnalysis/src/SampleAnalysis.WebUI/vite.config.ts
@@ -3,5 +3,13 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+  plugins: [preact()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5066',
+        secure: false,
+      },
+    },
+  },
 });
